cd54f3c7a651b48e36fb54cce19df2d2a3e95dfa
Carlos <luchillo17@gmail.com>
2021-05-08 18:49:37 -0500

    fix: Setup I18N

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+gem 'rails-i18n'
+
 # Auth
 gem 'devise', github: 'heartcombo/devise', branch: 'ca-omniauth-2'
 gem 'omniauth'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+  end
+
+  def extract_locale
+    parsed_locale = params[:locale]
+    browser_locales = request.env['HTTP_ACCEPT_LANGUAGE'].to_s.split(",").filter_map do |lang| 
+      l = lang.split(";q=")[0]
+      l if locale_available?(l)
+    end
+
+    return parsed_locale if locale_available?(parsed_locale)
+   
+    browser_locales.!empty? ? browser_locales.first : nil
+  end
+
+  def locale_available?(locale)
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
+  end
 end
