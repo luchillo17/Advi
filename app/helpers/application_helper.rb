37ad5b62da1eb5fce0c92f54eb7166a01d50bd33
Carlos <luchillo17@gmail.com>
2021-05-24 01:02:18 -0500

    feat: Add link_to_with_locale helper for I18n locale query param

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def link_to_with_locale(*args, &block)
+    locale_param = params.permit(:locale)
+    return link_to(*args, &block) unless locale_param.has_key?(:locale)
+
+    # If block means the name is the block and the url position shifts to the left
+    path_index = block_given? ? 0 : 1
+
+    uri = URI.parse(args[path_index])
+    uri.query = [uri.query, locale_param.to_query].compact.join('&')
+    args[path_index] = uri.to_s
+
+    link_to(*args, &block)
+  end
 end
